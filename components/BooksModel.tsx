1bf643212f9e3a747afce1e24a286b7a3c9fb37a
Arjun Teotia <arjun.teotia@myhelp.co.uk>
2023-05-17 15:52:59 +0100

    implemented pagination

---
+++ components/BooksModel.tsx
@@ -0,0 +1,101 @@
+export interface BooksResult {
+    kind: string;
+    totalItems: number;
+    items?: (ItemsEntity)[] | null;
+}
+export interface ItemsEntity {
+    kind: string;
+    id: string;
+    etag: string;
+    selfLink: string;
+    volumeInfo: VolumeInfo;
+    saleInfo: SaleInfo;
+    accessInfo: AccessInfo;
+    searchInfo: SearchInfo;
+}
+export interface VolumeInfo {
+    title: string;
+    authors?: (string)[] | null;
+    publisher: string;
+    publishedDate: string;
+    description: string;
+    industryIdentifiers?: (IndustryIdentifiersEntity)[] | null;
+    readingModes: ReadingModes;
+    pageCount?: number | null;
+    printType: string;
+    categories?: (string)[] | null;
+    maturityRating: string;
+    allowAnonLogging: boolean;
+    contentVersion: string;
+    panelizationSummary: PanelizationSummary;
+    imageLinks: ImageLinks;
+    language: string;
+    previewLink: string;
+    infoLink: string;
+    canonicalVolumeLink: string;
+    subtitle?: string | null;
+    averageRating?: number | null;
+    ratingsCount?: number | null;
+}
+export interface IndustryIdentifiersEntity {
+    type: string;
+    identifier: string;
+}
+export interface ReadingModes {
+    text: boolean;
+    image: boolean;
+}
+export interface PanelizationSummary {
+    containsEpubBubbles: boolean;
+    containsImageBubbles: boolean;
+}
+export interface ImageLinks {
+    smallThumbnail: string;
+    thumbnail: string;
+}
+export interface SaleInfo {
+    country: string;
+    saleability: string;
+    isEbook: boolean;
+    listPrice?: ListPriceOrRetailPrice | null;
+    retailPrice?: ListPriceOrRetailPrice1 | null;
+    buyLink?: string | null;
+    offers?: (OffersEntity)[] | null;
+}
+export interface ListPriceOrRetailPrice {
+    amount: number;
+    currencyCode: string;
+}
+export interface ListPriceOrRetailPrice1 {
+    amount: number;
+    currencyCode: string;
+}
+export interface OffersEntity {
+    finskyOfferType: number;
+    listPrice: ListPriceOrRetailPrice2;
+    retailPrice: ListPriceOrRetailPrice2;
+    giftable: boolean;
+}
+export interface ListPriceOrRetailPrice2 {
+    amountInMicros: number;
+    currencyCode: string;
+}
+export interface AccessInfo {
+    country: string;
+    viewability: string;
+    embeddable: boolean;
+    publicDomain: boolean;
+    textToSpeechPermission: string;
+    epub: EpubOrPdf;
+    pdf: EpubOrPdf;
+    webReaderLink: string;
+    accessViewStatus: string;
+    quoteSharingAllowed: boolean;
+}
+export interface EpubOrPdf {
+    isAvailable: boolean;
+    acsTokenLink?: string | null;
+}
+export interface SearchInfo {
+    textSnippet: string;
+}
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,41 +1,95 @@
-"use client"; 
-import { useState } from 'react';
+"use client";
+import React, { useState } from 'react';
+import { BooksResult } from './BooksModel';
+import { Loader } from './Loader';
 
 const SearchBar = () => {
+  const [currentPage, setcurrentPage] = useState(0);
+  const [maxResults, setmaxResults] = useState(5);
+  const [showLoader, setshowLoader] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
-  const [results, setResults] = useState<string[]>([]);
+  const [results, setResults] = useState<BooksResult>();
+  const displayOptions = [5, 10, 20]
+
+  React.useEffect(() => {
+    handleSearch()
+  }, [currentPage, maxResults])
 
   const handleSearch = async () => {
-    const response = await fetch(`http://localhost:3003/search?keyword=${searchQuery}`);
-    const data = await response.json();
-    console.error("data.results.items", data.items)
-    setResults(data.items);
+    if(searchQuery) {
+      setshowLoader(true)
+      const response = await fetch(`http://localhost:3003/search?keyword=${searchQuery}&maxResults=${maxResults}&startIndex=`+(currentPage*maxResults));
+      const data = await response.json();
+      setResults(data);
+      setshowLoader(false)
+
+    }
   };
+  const handleDisplayFilter = (event) => {
+    setcurrentPage(0)
+    setmaxResults(event.target.value)
+  }
+  const handlePrev = () => {
+    if(currentPage > 0) {
+      setcurrentPage(currentPage-1)
+    }
+  }
+  const handleNext = () => {
+    setcurrentPage(currentPage+1)
+
+  }
+  const handleChange = (event) => {
+    if(event.target.value > 1) {
+      setcurrentPage(event.target.value-1)
+    } else {
+      setcurrentPage(0)
+    }
+  }
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="border border-gray-300 rounded px-4 py-2"
-      />
-      <button
-        type="button"
-        onClick={handleSearch}
-        className="bg-blue-500 text-white px-4 py-2 rounded ml-2"
-      >
-        Search
-      </button>
-      <p>Total Results - {results.length}</p>
-      <ul className="mt-4">
-        {results.map((result, index) => (
-          <li key={index} className="text-gray-700 border border-gray-300 px-4 py-3">
-            {result.volumeInfo.title}
-          </li>
-        ))}
-      </ul>
+      <div className='justify-center flex'>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="border border-gray-300 rounded px-4 py-2"
+        />
+        <button
+          type="button"
+          onClick={handleSearch}
+          className="bg-blue-500 text-white px-4 py-2 rounded ml-2"
+        >
+          Search
+        </button>
+
+      {showLoader && <Loader />}
+      </div>
+      {results && results.totalItems > 0 &&
+        <div className='mt-2'>
+          Total Results - {results && results.totalItems.toString()} &nbsp;  Show - &nbsp;
+          <select className='mr-48' onChange={handleDisplayFilter}>
+            {displayOptions.map((val) => (<option key={val} value={val}>{val}</option>))}
+          </select>
+          <div className='float-right'>
+
+            <button className='ml-2 mr-2 text-indigo-700 underline' onClick={handlePrev}>Prev</button>
+            {/* <button className='ml-2 mr-2 text-indigo-700 underline' onClick={() => setcurrentPage(0)}>1</button>
+            <button className='ml-2 mr-2 text-indigo-700 underline' onClick={() => setcurrentPage(1)}>2</button> */}
+            <input className='w-12 text-center' value={currentPage+1} onChange={handleChange} />
+            <button className='ml-2 mr-2 text-indigo-700 underline' onClick={handleNext}>Next</button>
+          </div>
+        </div>}
+      <div className="bg-white space-y-4 mt-4 flex-1">
+        <ul className="">
+          {results?.items?.map((result, index) => (
+            <li key={index} className="text-gray-700 border border-gray-300 px-4 py-3">
+              {result.volumeInfo.authors?.join(",")} - {result.volumeInfo.title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
